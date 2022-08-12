5124d2194da78c9e31f155fbe4da4b02f80aab55
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-12 18:49:31 +0900

    対戦するプレイヤーを格納する場所作成

---
+++ Assets/Scripts/PlayersStock.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayersStock : MonoBehaviour
+{
+    // 今回対戦するプレーヤー
+    public static Queue<Human> players;
+}
