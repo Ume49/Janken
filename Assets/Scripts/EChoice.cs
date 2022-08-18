d5533772fefeafd49f41547206d62022b408375a
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-18 18:54:57 +0900

    順番の変更と勝敗の判定をするグローバル関数を追加

--- Assets/Scripts/EChoice.cs
+++ Assets/Scripts/EChoice.cs
@@ -2,8 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EChoice{
+// じゃんけんの手
+// 次のやつが前のやつに勝てる順番
+public enum EChoice : int{
     Rock,
+    Paper,
     Scisors,
-    Paper
+    length
+}
+
+// じゃんけんの勝敗を判定する関数
+public class Choice_Judge{
+    // -1 : 引き分け
+    // それ以外 choice_数字 の勝った方の数字を返す
+    static public int Judge(EChoice choice_0, EChoice choice_1){
+        
+        // 引き分け
+        if(choice_0 == choice_1) return -1;
+
+        // choice_0 の「次のやつ」を計算
+        // Scissors + 1 = Rockとなるようにループの考慮もする
+        EChoice next_0 = (EChoice)((int)(choice_0 + 1)%(int)EChoice.length);
+
+        if (choice_1 == next_0){
+            // 列挙型の定義より、一致してたらchoice_1の勝利
+            return 1;
+        }
+        else{
+            // 残るのはchoice_0の勝利しかない
+            return 0;
+        }
+    }
 }
