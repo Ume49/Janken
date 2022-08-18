a10ce99ead1f8c7ba905ab220e4fd6a338660681
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-18 18:53:59 +0900

    管理機編集

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,51 +4,56 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] GameObject player_ui;
-    [SerializeField] GameObject enemy_ui;
-    
-    [SerializeField] Transform ui_parent;
+    // 対戦プレイヤー
+    List<Human> players_;
 
-    // 現在処理しているプレイヤー
-    Human currentPlayHuman;
+    // 現在処理しているプレイヤーを指し示すインデックス
+    int current_player_index_ = -1;
+
+    // プレイヤーのGUIを表示する上で親に設定させるオブジェクト
+    [SerializeField] GameObject gui_parent_;
+
+    // 結果を表示する
+    [SerializeField] ResultDisplayer resultDisplayer_;
+
+    void Awake(){
+        players_ = new List<Human>();
+    }
 
     void Start() {
         StartHumanChoice();
     }
 
-    void StartHumanChoice(){
+    void StartHumanChoice(){ Debug.Log("PlayerManager.StartHumanChoice()");
         // 次のプレイヤーを取得
-        var human = PlayersStock.players.Dequeue();
-        // TODO: 空の時の処理
+        players_.Add(PlayersStock.players.Dequeue());
+        current_player_index_++;
 
-        GameObject instanceGameObject = null;
-
-        // 種類に対応したUIを追加
-        switch(human.kind){
-            case Human.Kind.Player:
-                instanceGameObject = (GameObject)Instantiate(player_ui, player_ui.transform);
-                break;
-            case Human.Kind.Enemy:
-                instanceGameObject = (GameObject)Instantiate(enemy_ui, enemy_ui.transform);
-                break;
-        }
-
-        // 追加したUIを子要素として登録
-        instanceGameObject.transform.parent = ui_parent;
-
-        // 手を選ぶ処理をするため登録
-        currentPlayHuman = human;
+        // ユーザーに現在プレイヤーの判定をしていることを通知するGUIを表示させる
+        players_[current_player_index_].Display(gui_parent_);
     }
 
     void Update(){
-        EChoice result;
+        if(players_[current_player_index_].Choice()){
+            if(PlayersStock.players.Count > 0){
+                // まだプレイヤーが残っているなら取得して手の判定をさせる
+                StartHumanChoice();
+                return;
+            }
 
-        if(currentPlayHuman.Choice(out result)){
-            // TODO: 結果を格納
+            // 手を全部決めたら勝敗判定
+            int result = Choice_Judge.Judge(players_[0]._choice, players_[1]._choice);
 
-            StartHumanChoice();
+            // 結果表示
+            resultDisplayer_.Display(players_, result);
 
-            // TODO: 手を全部決めたら勝敗判定
+            // DontDestroyOnなんとかに設定されているプレイヤーたちを削除
+            // *List<T>の挙動を完全に把握してるわけではないので一応ケツから消す
+            Destroy(players_[1].gameObject);
+            Destroy(players_[0].gameObject);
+
+            // このコンポーネントを削除してUpdateを呼ばれないようにする
+            Destroy(this);
         }
     }
 }
