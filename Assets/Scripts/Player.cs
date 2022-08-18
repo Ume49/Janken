5bea4fee23d61ea1ede15ae60f380d50b93b440d
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-18 18:53:28 +0900

    ユーザー操作のプレイヤー完成

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Human
 {
-    public EChoice choice_;
+    // 手を決定し終わっているか
+    public bool isDecided_ = false;
 
-    EChoice Dummy(){
-        return choice_;
+    public override bool Choice()
+    {   Debug.Log("Player.Choice()");
+        
+        if(isDecided_ == true)
+        {
+            DisplayChoice();
+
+            _result_text.gameObject.SetActive(true);
+            
+            // ボタンを非表示にする
+            // 兄弟関係にあるオブジェクトから無理やり参照する
+            _result_text.gameObject.transform.parent.GetChild(1).gameObject.SetActive(false);
+
+            return true;
+        }
+        return false;
+    }
+
+    public void SetChoice(EChoice choice){ Debug.Log("Player.SetChoice(choice)");
+        _choice = choice;
+
+        // 手を決定したので変更
+        isDecided_ = true;
     }
 }
